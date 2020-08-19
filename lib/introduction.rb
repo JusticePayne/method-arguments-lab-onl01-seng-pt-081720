9a5ff3969ce2c557a38e0634fff15f3cd534e19b
Justice Payne <spikepayne@yahoo.com>
2020-08-19 17:43:14 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,3 +1,9 @@
 def introduction (name)
   puts "Hi, my name is #{name}."
 end
+
+def introduction_with_language (name, phrase)
+  puts "Hi, my name is #{name} and I'm learning how to code."
+
+end
+
